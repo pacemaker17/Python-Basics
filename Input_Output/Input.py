a9442a504a5f7412805114fcca4a2b2403892352
pacemaker17 <pacemaker753@gmail.com>
2018-09-17 00:17:34 +0530

    Print prime numbers between two numbers

--- Input_Output/Input.py
+++ Input_Output/Input.py
@@ -2,3 +2,19 @@ a =10; b=20
 print("Value of a is {} and value of b is {}".format(a,b))
 
 
+num = input("Enter a number")
+print(num)
+
+
+
+index1 = 10
+index2 = 50
+print("Prime numbers between {} and {} are :".format(index1, index2))
+for num in range(index1, index2+1):
+    if num > 1:
+        isDivisible  = False
+        for index in range(2, num):
+            if num % index == 0:
+                isDivisible = True
+        if not isDivisible:
+            print(num, end=" ")
